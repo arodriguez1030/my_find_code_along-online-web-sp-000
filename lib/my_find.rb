a61285ea690f730a7f17abb0e8f0b49d9443cdd4
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-17 13:28:51 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
 def my_find(collection)
-
+i = 0
+block_array = []
+while i< collection.length
+return collection[i]
+if yield(collection[i])
+block_array << yield(collection[i])
+i += 1
+end
 end
